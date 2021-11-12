5b026ab9dd71838c252ea09b9104862156d2abfe
Björn Rudner <rudnerbjoern@hotmail.com>
2021-11-12 11:45:58 +0100

    Initial release

---
+++ de.dict.br-landscape.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2021 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2021-11-12
+ *
+ * Localized data
+ */
+
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+    // Dictionary entries go here
+    'Class:FunctionalCI/Attribute:system_landscape' => 'System landscape',
+    'Class:FunctionalCI/Attribute:system_landscape+' => 'System landscape the system is located in',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:dev' => 'Development',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:dev+' => 'Development system',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:qas' => 'Quality assurance',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:qas+' => 'Quality assurance system',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:prd' => 'Production',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:prd+' => 'Production system',
+));
---
+++ en.dict.br-landscape.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2021 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2021-11-12
+ *
+ * Localized data
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+    // Dictionary entries go here
+    'Class:FunctionalCI/Attribute:system_landscape' => 'System landscape',
+    'Class:FunctionalCI/Attribute:system_landscape+' => 'System landscape the system is located in',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:dev' => 'development',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:dev+' => 'Development system',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:qas' => 'quality assurance',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:qas+' => 'Quality assurance system',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:prd' => 'production',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:prd+' => 'Production system',
+));
