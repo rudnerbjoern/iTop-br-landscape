20f315d6cb0704a9d953c582bb8f5e7e2fd49872
Björn Rudner <rudnerbjoern@hotmail.com>
2022-12-05 15:25:35 +0100

    add Landscape to ApplicationSolution

--- module.br-landscape.php
+++ module.br-landscape.php
@@ -3,14 +3,14 @@
 /**
  * @copyright   Copyright (C) 2021 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2021-11-12
+ * @version     2022-12-05
  *
  * iTop module definition file
  */
 
 SetupWebPage::AddModule(
     __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-landscape/0.2.0',
+    'br-landscape/0.3.0',
     array(
         // Identification
         //
