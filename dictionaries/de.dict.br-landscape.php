cdb37c902789b650bfa5e68dd2e21ca4ae4087c8
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-08-27 15:16:10 +0200

    move dictionary in separate folder

---
+++ dictionaries/de.dict.br-landscape.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2024 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2024-08-27
+ *
+ * Localized data
+ */
+
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+    'Class:FunctionalCI/Attribute:system_landscape' => 'Systemlandschaft',
+    'Class:FunctionalCI/Attribute:system_landscape+' => 'Systemlandschaft, in der sich das System befindet',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:dev' => 'Entwicklung',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:dev+' => 'Entwicklungssystem',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:qas' => 'Qualitätssicherung',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:qas+' => 'Qualitätssicherungssystem',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:prd' => 'Produktion',
+    'Class:FunctionalCI/Attribute:system_landscape/Value:prd+' => 'Produktionssystem',
+));
