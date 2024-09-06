34f2c6e6da2d7818a3cab7e97083b19d9d8176d1
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-09-06 18:06:17 +0200

    enhance compatibility to iTop 3.1

--- br-landscape/module.br-landscape.php
+++ br-landscape/module.br-landscape.php
@@ -3,14 +3,14 @@
 /**
  * @copyright   Copyright (C) 2021-2024 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-09-02
+ * @version     2024-09-06
  *
  * iTop module definition file
  */
 
 SetupWebPage::AddModule(
     __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-landscape/1.1.1',
+    'br-landscape/3.1.2',
     array(
         // Identification
         //
@@ -20,7 +20,7 @@ SetupWebPage::AddModule(
         // Setup
         //
         'dependencies' => array(
-            '(itop-config-mgmt/2.5.0 & itop-config-mgmt/<3.0.0)||itop-structure/3.0.0',
+            'itop-structure/3.0.0',
             'itop-virtualization-mgmt/0.0.0'
         ),
         'mandatory' => false,
@@ -29,9 +29,7 @@ SetupWebPage::AddModule(
 
         // Components
         //
-        'datamodel' => array(
-            'model.br-landscape.php',
-        ),
+        'datamodel' => array(),
         'webservice' => array(),
         'data.struct' => array(
             // add your 'structure' definition XML files here,
