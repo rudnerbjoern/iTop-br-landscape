97748b96267b9cdddcc54bdec274b197e0a68ae6
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-09-06 17:59:11 +0200

    enhance compatibility to iTop 2.7

--- br-landscape/module.br-landscape.php
+++ br-landscape/module.br-landscape.php
@@ -3,14 +3,14 @@
 /**
  * @copyright   Copyright (C) 2021-2024 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-09-02
+ * @version     2024-09-06
  *
  * iTop module definition file
  */
 
 SetupWebPage::AddModule(
     __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-landscape/1.1.1',
+    'br-landscape/2.7.2',
     array(
         // Identification
         //
