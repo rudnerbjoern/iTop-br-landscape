531ffa579dd49174b38c8db41d1cdda4ae8eb69a
Bjoern Rudner <rudnerbjoern@hotmail.com>
2025-06-12 14:35:02 +0200

    set compatibility to iTop 3.2

--- br-landscape/dictionaries/de.dict.br-landscape.php
+++ br-landscape/dictionaries/de.dict.br-landscape.php
@@ -3,12 +3,12 @@
 /**
  * @copyright   Copyright (C) 2021-2025 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2025-04-24
+ * @version     2025-06-12
  *
  * Localized data
  */
 
- /** @disregard P1009 Undefined type Dict */
+/** @disregard P1009 Undefined type Dict */
 Dict::Add('DE DE', 'German', 'Deutsch', array(
     'Class:FunctionalCI/Attribute:system_landscape' => 'Systemlandschaft',
     'Class:FunctionalCI/Attribute:system_landscape+' => 'Systemlandschaft, in der sich das System befindet',
